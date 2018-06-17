55706b6531af302376b5f84226e377db2630cdf3
トマト <@DESKTOP-E0F80M6>
2018-06-17 15:00:56 +0900

    inner-company proejct github test

--- tomatoMRReservation_1806121038/src/main/java/com/jptomato/tomatoMRReservation/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jptomato.tomatoMRReservation.mapper;
-
-import com.jptomato.tomatoMRReservation.model.User;
-
-public interface UserMapper {
-	public User getUserByUserId(Integer userId);
-}
